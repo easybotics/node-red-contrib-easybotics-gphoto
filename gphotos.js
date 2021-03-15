f2a5b6c2002038197b3366216c961a932f354bbb
Northern Man <sgracey@Heisenberg.local>
2021-03-14 21:08:10 -0400

    Update to most recent upload-gphotos
    Fix for login issues

--- gphotos.js
+++ gphotos.js
@@ -1,4 +1,6 @@
-const gphoto = require('upload-gphotos').default;
+const {
+  GPhotos
+} = require('upload-gphotos');
 const fs = require('fs');
 var streamifier = require('./lib/streamifier.js');
 
@@ -7,12 +9,8 @@ module.exports = function(RED) {
     RED.nodes.createNode(this, config);
     const node = this;
 
-
     node.loginAttempt = false;
-    node.account = new gphoto({
-      username: node.credentials.username,
-      password: node.credentials.password
-    });
+    node.gphotos = new GPhotos();
   }
 
   function UploadPhoto(config) {
@@ -24,31 +22,56 @@ module.exports = function(RED) {
     const photoAccount = RED.nodes.getNode(config.account);
 
     async function upload(fileName, albumName, payload) {
-      if (!photoAccount.account.userId && !photoAccount.loginAttempt) {
+      if (!photoAccount.gphotos.userId && !photoAccount.loginAttempt) {
         photoAccount.loginAttempt = true;
-        await photoAccount.account.login();
+        node.log('Google login ' + photoAccount.credentials.username);
+        await photoAccount.gphotos.signin({
+          username: photoAccount.credentials.username,
+          password: photoAccount.credentials.password
+        });
       }
 
       let photo;
       if (payload.image === undefined) {
-        photo = await photoAccount.account.upload(fileName);
+        photo = await photoAccount.gphotos.upload({
+          stream: fs.createReadStream(fileName),
+          size: (await fs.promises.stat(fileName)).size,
+          filename: fileName
+        });
       } else {
-        photo = await photoAccount.account.uploadFromStream(streamifier.createReadStream(payload.image), payload.image.length, fileName);
+        photo = await photoAccount.gphotos.upload({
+          stream: streamifier.createReadStream(payload.image),
+          size: payload.image.length,
+          filename: fileName
+        });
       }
-      const album = await photoAccount.account.searchOrCreateAlbum(albumName);
-
-      const id = await album.addPhoto(photo);
+      node.log('photo id ' + photo.id);
+      // const album = await photoAccount.account.searchOrCreateAlbum(albumName);
+      const album =
+        (await photoAccount.gphotos.searchAlbum({
+          title: albumName
+        })) || (await photoAccount.gphotos.createAlbum({
+          title: albumName
+        }));
+      node.log('album id ' + album.id);
+      await album.append(photo);
       if (payload.description) {
-        await photo.modifyDescription(payload.description);
+        node.log("description " + payload.description);
+        await photo.modifyDescription({
+          description: payload.description
+        });
       }
       if (payload.timestamp) {
-        await photo.modifyCreatedDate(payload.timestamp);
+        node.log("timestamp " + payload.timestamp);
+        await photo.modifyCreatedDate({
+          createdDate: new Date(payload.timestamp)
+        });
       }
     }
 
     node.on('input', function(msg) {
-      upName = undefined;
-      upAlbum = undefined;
+      var upName = undefined;
+      var upAlbum = undefined;
 
       if (typeof msg.payload === "string") {
         upName = msg.payload;
@@ -83,6 +106,15 @@ module.exports = function(RED) {
             shape: 'ring',
             fill: 'green'
           });
+        }).catch(function(err) {
+          photoAccount.loginAttempt = false;
+          console.error(err);
+          node.status({
+            text: 'ERROR: ' + err.message,
+            shape: 'dot',
+            fill: 'red'
+          });
+          node.error('ERROR: ' + err.message, msg);
         });
       } else {
         node.warn('Not uploading, missing filename or album name.');
